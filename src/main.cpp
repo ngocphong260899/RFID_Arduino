f3ceff4d8889fc241186f5201f43d787338816bd
ngocphong <ngocphong260899@gmail.com>
2021-03-19 07:37:29 +0700

    fix serial.println(indt)

--- src/main.cpp
+++ src/main.cpp
@@ -2,85 +2,81 @@
 #include <RFID.h>
 #include <Wire.h>
 
-
-//Khai báo chân SDA và RST
 #define SS_PIN 10
 #define RST_PIN 9
 
-//Khai báo màn hình LCD và RFID
-
 RFID rfid(SS_PIN, RST_PIN);
 char msg[100] = " ";
-char* stt = "status";
-int serNum[5];              //Biến lưu id thẻ đang đọc
-int cards[][5] =            //Những thẻ được phép mở
-{
-  {215, 75, 253, 103, 6},
-  {96, 90, 124, 108, 42}
-};
+char *stt = "status";
+int serNum[5];
+int cards[][5] =
+    {
+        {215, 75, 253, 103, 6},
+        {96, 90, 124, 108, 42}};
 
-String idin;                //Biến lưu id thẻ đang đọc để xuất lcd
+String idin;
 
-bool access = false;        //Biến lưu quyền truy nhập
+bool access = false;
+
 void disp(bool check);
 void setup()
 {
-  Serial.begin(9600);       //Mở Serial có Baudrate 9600
-  SPI.begin();              //Khởi tạo giao tiếp SPI
-  rfid.init();              //Khởi tạo RFid
-
-
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.init();
 }
 
 void loop()
 {
-  if (rfid.isCard())        //Nếu có thẻ 
-  {
-    if (rfid.readCardSerial())  //Đọc thẻ
+  
+    if (rfid.isCard())
     {
-      //Chuyển đổi ID thẻ sang String
-      idin = "";
-      // for (int i = 0; i < sizeof(rfid.serNum); i++ )
-      // {
-      //   idin += String(rfid.serNum[i]);
-      //   Serial.print(rfid.serNum[i]);
-      //   Serial.print(" ");
-      // }
-
-      //So sánh ID thẻ với ID có thể truy nhập
-      for (int x = 0; x < sizeof(cards); x++)
+      if (rfid.readCardSerial())
       {
-        for (int i = 0; i < sizeof(rfid.serNum); i++ )
+        //Chuyển đổi ID thẻ sang String
+        idin = "";
+        for (int i = 0; i < sizeof(rfid.serNum); i++)
         {
-          access = true;
-          //Nếu khác thì gán quyền truy cập = False
-          if (rfid.serNum[i] != cards[x][i])
-          {
-            access = false;
-            break;
-          }
+          idin += String(rfid.serNum[i]);
+          // Serial.print(rfid.serNum[i]);
         }
-        if (access) break;
+        Serial.println(idin);
+        //So sánh ID thẻ với ID có thể truy nhập
+        for (int x = 0; x < sizeof(cards); x++)
+        {
+          for (int i = 0; i < sizeof(rfid.serNum); i++)
+          {
+            access = true;
+            //Nếu khác thì gán quyền truy cập = False
+            if (rfid.serNum[i] != cards[x][i])
+            {
+              access = false;
+              break;
+            }
+          }
+          if (access)
+            break;
+        }
       }
+      disp(access);
     }
-    disp(access); 
+    rfid.halt(); //Dừng đọc
   }
-  rfid.halt();    //Dừng đọc
-}
+
 
 void disp(bool check)
 {
-  
-  if (check)            //Nếu đúng ID
+  if (check)
   {
-    sprintf(msg,"{\"%s\":%d}",stt,1);
-    Serial.println(msg);
+    // sprintf(msg,"{\"%s\":%d}",stt,1);
+    // Serial.println(msg);
+    // open door in here source code
+    Serial.println("Open door");
   }
-  else                  //Ngược lại
+  else
   {
-    sprintf(msg,"{\"%s\":%d}",stt,0);
-    Serial.println(msg);
+    // sprintf(msg,"{\"%s\":%d}",stt,0);
+    // Serial.println(msg);
   }
-  Serial.println();
-  delay(1000);          //Chờ 1s
+  delay(1000);
 }
